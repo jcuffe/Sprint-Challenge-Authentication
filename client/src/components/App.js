f9a098b8bfbf60bf61936a03044cc8f646479e4e
Jared Cuffe <jcuffe@gmail.com>
2018-05-18 11:48:28 -0700

    All routes available

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router'
+import { Register, Login } from './Auth'
+import Jokes from './Jokes'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Route path='/register' component={Register} />
+        <Route path='/login' component={Login} />
+        <Route path='/jokes' component={Jokes} />
       </div>
     )
   }
 }
-
-export default App
