2beb94ef19d39306bf603eda3a019d1b68d6584b
Jared Cuffe <jcuffe@gmail.com>
2018-05-18 11:02:11 -0700

    Register endpoint implemented

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,10 +1,11 @@
 const User = require('../models/userModels');
 const bcrypt = require('bcrypt');
 
-const createUser = (req, res) => {
+const createUser = (req, res, next) => {
   const { username, password } = req.body;
-  // create user takes in the username and password and saves a user.
-  // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  User.create({ username, password })
+    .then(user => res.send(user))
+    .catch(err => next(err))
 };
 
 module.exports = {
